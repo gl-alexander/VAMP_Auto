c4b3cc5998300c884a303e3b17708b272f583255
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2022-04-04 16:43:52 +0300

    Create AppDbContext.cs

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace VAMP_Auto.Data
+{
+    public class AppDbContext : DbContext
+    {
+    }
+}
