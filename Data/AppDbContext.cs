bee75708fb2904ea56a80e7163d1749d3489a579
TSCHUPACABRA <102968737+TSCHUPACABRA@users.noreply.github.com>
2022-04-05 15:22:31 +0300

    Update AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Web;
 using VAMP_Auto.Models;
@@ -8,29 +8,33 @@ using VAMP_Auto.Models;
 
 namespace VAMP_Auto.Data
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext : DbContext
     {
         public DbSet<Car> Cars { set; get; }
         public DbSet<User> Users { set; get; }
         public DbSet<Query> Queries { set; get; }
-        public AppDbContext():base()
+        public AppDbContext() : base()
         {
 
         }
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=vamp;Trusted_Connection=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Query>()
                 .HasKey(c => new { c.UserId, c.CarId });
 
-            modelBuilder.Entity<User>()
-                .HasMany(c => c.Queries)
-                .WithRequired()
-                .HasForeignKey(c => c.UserId);
-
-            modelBuilder.Entity<Car>()
-                .HasMany(c => c.Queries)
-                .WithRequired()
+            modelBuilder.Entity<Query>()
+                .HasOne(q=>q.Car)
+                .WithMany(q2 => q2.Queries)
                 .HasForeignKey(c => c.CarId);
+
+            modelBuilder.Entity<Query>()
+                .HasOne(q=>q.User)
+                .WithMany(q3 => q3.Queries)
+                .HasForeignKey(c => c.UserId);
         }
     }
 }
