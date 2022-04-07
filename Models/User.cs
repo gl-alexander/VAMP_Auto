ccc719c8050427a77628d5c8c55eb1778df8c9e4
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2022-04-07 11:32:27 +0300

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,19 +1,36 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace VAMP_Auto.Models
+namespace VAMPAutoCore.Models
 {
     public class User
     {
         public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
         public string Username { set; get; }
+
+        [Required(ErrorMessage = "Required.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
         public string UCN { get; set; }
-        public string  Phone { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string Email { get; set; }
 
         public virtual ICollection<Query> Queries { set; get; }
